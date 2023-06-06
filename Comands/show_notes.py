fd1c098006d8b806d8f7227ada1b584a58786007
Fiodar <fiodarspesian@mail.ru>
2023-06-06 11:10:02 +0300

    add changes class Show

--- Comands/show_notes.py
+++ Comands/show_notes.py
@@ -6,8 +6,5 @@ class Show(Comand):
     description = "Show list of notes."
 
     def action(self, notebook=None):
-        pass
-        if notebook is None:
-            print("You don't have notes.")
         for el in notebook:
             print(el)
--- note.py
+++ note.py
@@ -5,8 +5,8 @@ class Note:
     creation_day = datetime.datetime.today().strftime("%d.%m.%Y %H:%M:%S")
 
     def __init__(self):
-        self.event_day = input("Enter day of event in format dd mm yyyy: ")
-        self.name = input("Enter name of note: ")
+        self.name = input("Enter TITLE of note: ")
+        self.event_day = input("Enter EVENT DAY in format dd.mm.yyyy: ")
         self.note_content = input("Enter content of note: ")
         print("Note create successful!")
 
